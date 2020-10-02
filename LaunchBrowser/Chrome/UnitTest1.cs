f6c90ed38a579adf2fb357497c2ffc461182aaf2
g0pinath-metricon <65883112+g0pinath-metricon@users.noreply.github.com>
2020-10-02 19:42:40 +1000

    add

---
+++ LaunchBrowser/Chrome/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+
+namespace LaunchBrowser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestChrome()
+        {
+            IWebDriver driver = null;
+            try
+            {
+                driver = new ChromeDriver(@"C:\Users\GopiThiruvengadam\source\repos\LaunchBrowser\LaunchBrowser");
+                driver.Url = "https://www.foxnews.com/";
+                driver.Manage().Window.Maximize();
+                driver.Navigate();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception " + e.ToString());
+            }
+        }
+    }
+}
---
+++ LaunchBrowser/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LaunchBrowser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LaunchBrowser")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39ee8198-0d2d-4db2-b35e-9db62f5316b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
